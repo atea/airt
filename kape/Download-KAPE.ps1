87e5985e185d93e5f686d9a473cc53609228ad23
godstoge <59265829+godstoge@users.noreply.github.com>
2020-11-15 00:14:51 +0100

    Update Download-KAPE.ps1

--- kape/Download-KAPE.ps1
+++ kape/Download-KAPE.ps1
@@ -16,16 +16,13 @@ write-host ""
 Write-Host " The script will download KAPE and extract it to the current working directory."
 write-host ""
 
-
-
 $kapedestfolder = "C:\airt\"
+$kapetkapefolder = "C:\airt\KAPE\targets\!local"
+$kapeoutputfolder = "C:\airt\KAPE_OUT"
 
-if (Test-Path $kapedestfolder) {
-	Write-Host "Destination folder exists."
-	} else {
-	Write-Host "Creating destination folder."
-	New-Item -ItemType Directory -Force -Path $kapedestfolder | out-null
-	}
+if(New-Item -ItemType Directory -Path $kapedestfolder -erroraction 'silentlycontinue')   { Write-Host "$kapedestfolder created"  }
+if(New-Item -ItemType Directory -Path $kapetkapefolder -erroraction 'silentlycontinue')  { Write-Host "$kapetkapefolder created" }
+if(New-Item -ItemType Directory -Path $kapeoutputfolder -erroraction 'silentlycontinue') { Write-Host "$kapeoutputfolder created"}
 
 Write-Host "Downloading KAPE."
 $destFile = Join-Path -Path $kapedestfolder -ChildPath 'kape.zip'
@@ -42,5 +39,10 @@ if (Test-Path $kapedestfolder"\KAPE\kape.exe") {
 	} else {
 	write-host "Can not find kape.exe.... o.O"
 	}
+
+New-Item -ItemType Directory -Force -Path $kapetkapefolder | out-null
 	
 Write-Host "Downloading KAPE Atea-files."
+$destFile = Join-Path -Path $kapedestfolder -ChildPath 'KAPE\targets\!local\!Atea_collection.tkape'
+$tkapeUrl = "https://raw.githubusercontent.com/ateanorge/airt/master/kape/!Atea_collection.tkape"
+Invoke-WebRequest -Uri $tkapeUrl -OutFile $destFile -ErrorAction:Stop -UseBasicParsing
